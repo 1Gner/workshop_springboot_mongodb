7ba117049c290fa08dee8593626b6e0935e15104
joao <joao.orosario01@gmail.com>
2023-11-23 21:08:40 -0300

    connect mongo e service

--- src/main/java/com/joao/workshopmongo/resource/UserResource.java
+++ src/main/java/com/joao/workshopmongo/resource/UserResource.java
@@ -4,22 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joao.workshopmongo.domain.User;
+import com.joao.workshopmongo.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
+	
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1","maria","m@gmail.com");
-		User alex = new User("2","alex","a@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria,alex));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
