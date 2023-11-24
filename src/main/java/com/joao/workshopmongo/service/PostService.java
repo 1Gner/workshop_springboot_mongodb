2d26eb36719978c65d57baa106a05f614b696c3d
joao <joao.orosario01@gmail.com>
2023-11-24 18:23:37 -0300

    Retornando id post

---
+++ src/main/java/com/joao/workshopmongo/service/PostService.java
@@ -0,0 +1,24 @@
+ package com.joao.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joao.workshopmongo.domain.Post;
+import com.joao.workshopmongo.repository.PostRepository;
+import com.joao.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not Found"));
+	}
+	
+} 
